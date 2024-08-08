fc2ae5470444eeae2a458c6f0db2117afe1979c0
Kevin Lauer <kevin.lauer2019@gmail.com>
2024-08-08 17:32:15 -0500

    test of full viewport landing page on mobile

--- src/components/MobileTopContainer.jsx
+++ src/components/MobileTopContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MobileTopContainer = () => (
-  <div className="pb-5 px-2 d-block d-xl-none" style={{ paddingTop: "6.5rem" }}>
+  <div className="topContainer pb-5 px-2 d-block d-xl-none" style={{ paddingTop: "6.5rem" }}>
     <h1 className="display-4 font-monospace">I'm a freelance software developer who does business like a good car mechanic. My name is <span className="fw-bold">Kevin Lauer</span>.</h1>
   </div>
 );
