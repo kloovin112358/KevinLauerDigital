1c991101dc9665615d7ead0fcee2330ecaba780f
Kevin Lauer <kevin.lauer2019@gmail.com>
2024-08-08 17:29:02 -0500

    revised get a quote link

--- src/components/GetAQuoteSection.jsx
+++ src/components/GetAQuoteSection.jsx
@@ -4,7 +4,7 @@ import {Fade } from "react-awesome-reveal";
 const GetAQuoteSection = () => {
   return (
     <div className="mt-3 mt-lg-5" id="getAQuoteSection">
-        <h1 className="fw-bold display-4" id="hire">Get a <span className="text-primary">Quote</span></h1>
+        <h1 className="fw-bold display-4" id="quote">Get a <span className="text-primary">Quote</span></h1>
         <div className="fs-3 font-monospace mt-2 mt-lg-3">
           <Fade triggerOnce={true} duration={2000}>
           <span>Send me an email with your project details to <a href="mailto:ktlauer@proton.me?subject=Software Development Work Inquiry" target="_blank" rel="noreferrer">ktlauer@proton.me</a> and 
