94c51d5cc3668260b0770e1a0fa0979e6fe95a4b
Kevin Lauer <kevin.lauer2019@gmail.com>
2024-08-10 18:06:08 -0500

    attempt 2 to fix mobile animation bug

--- src/components/MobileTopContainer.jsx
+++ src/components/MobileTopContainer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MobileTopContainer = () => (
-  <div className="topContainer pb-5 px-2 d-block d-xl-none" style={{ paddingTop: "5.2rem" }}>
-    <h1 className="display-4 font-monospace">I'm a freelance software developer who does business like a good car mechanic. My name is <span className="fw-bold">Kevin Lauer</span>.</h1>
-  </div>
+  <>
+    <div className="pb-5 px-2 d-block d-xl-none" style={{ paddingTop: "5.2rem" }}>
+      <h1 className="display-4 font-monospace">I'm a freelance software developer who does business like a good car mechanic. My name is <span className="fw-bold">Kevin Lauer</span>.</h1>
+    </div>
+  </>
 );
 
 export default MobileTopContainer;
--- src/components/WhatICanDoSection.jsx
+++ src/components/WhatICanDoSection.jsx
@@ -8,28 +8,7 @@ function WhatICanDoSection() {
   return (
     <div className="pb-5">
     <h1 className="fw-bold display-4 text-center" id="do">What I Can <span className="text-primary">Do</span> (for you)</h1>
-    <div className="d-none d-lg-block">
-      <Fade duration={2000} triggerOnce={true}>
-        <div className="row mt-2 mt-lg-3">
-          <WhatICanDoCard 
-            icon="bi-window" 
-            title="Full-Stack Web Development" 
-            description="Complex web apps, static websites, I've got you. Built from scratch or changes to existing sites." 
-          />
-          <WhatICanDoCard 
-            icon="bi-mouse3" 
-            title="Process Automation" 
-            description="Tools to make your life less annoying, like scripts, macros, and RPA." 
-          />
-          <WhatICanDoCard 
-            icon="bi-braces" 
-            title="Miscellaneous" 
-            description="Other digital projects you might have. I might not have the necessary experience, but feel free to ask!" 
-          />
-        </div>
-      </Fade>
-    </div>
-    <div className="d-block d-lg-none">
+    <Fade duration={2000} triggerOnce={true}>
       <div className="row mt-2 mt-lg-3">
         <WhatICanDoCard 
           icon="bi-window" 
@@ -47,7 +26,7 @@ function WhatICanDoSection() {
           description="Other digital projects you might have. I might not have the necessary experience, but feel free to ask!" 
         />
       </div>
-    </div>
+    </Fade>
     <Fade duration={2000} triggerOnce={true}>
     <div className="text-center mt-5 mb-4">
       <h2 className="fw-bold display-6">How Your Project Could Look</h2>
