09ac8dc57c315eac46d9b6bc1cf168ba7658e3ae
Kevin Lauer <kevin.lauer2019@gmail.com>
2024-08-11 19:23:49 -0500

    attempted compression of images and videos

--- src/components/MacroSample.jsx
+++ src/components/MacroSample.jsx
@@ -7,7 +7,7 @@ const MacroSample = () => {
             <div className="mt-2 pb-4 text-center">
                 <h4>Excel Macro - Aggregating Timesheets and Calculating Labor Cost</h4>
             </div>
-            <video controls src={"/Macro_demo.mp4"} className="object-fit-contain d-block mx-auto" autoPlay style={{maxWidth:"100%", maxHeight:"35rem"}} muted></video>
+            <video controls src={"/Macro_demo.mp4"} className="object-fit-contain d-block mx-auto" autoPlay style={{maxWidth:"100%", maxHeight:"35rem"}} muted loading="lazy"></video>
         </div>
     </>
   );
